c65e27659997c19a3932534893c74a6cc6f93e4b
Alparslan <alparslanaydgn02@gmail.com>
2022-10-22 18:08:33 +0300

    Collections(Liste) eklendi

---
+++ Collections/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> isimler = new List<string> {"Alp","Arslan","Kasım" };
+            foreach (var name in isimler)
+            {
+                Console.WriteLine(name);
+            }
+            isimler.Add("Murat");
+            Console.WriteLine(isimler[3]);
+        }
+    }
+}
