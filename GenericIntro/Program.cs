1db84111f444bb8838e9a8613e0b91ad015591ed
Alparslan <alparslanaydgn02@gmail.com>
2022-10-22 18:09:55 +0300

    GenericIntro eklendi (Kendi veri yapımızı(liste) yaptık ve ekledik )

---
+++ GenericIntro/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Alp");
+            isimler.Add("Arslan");
+            isimler.Add("Aydoğan");
+
+            foreach (var isim in isimler.Items)
+            {
+                Console.WriteLine(isim);
+            }
+
+
+        }
+    }
+}
