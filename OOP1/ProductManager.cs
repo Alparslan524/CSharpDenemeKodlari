139be2cc5e48af0b0b15cdd517b917c586da32a0
Alparslan <alparslanaydgn02@gmail.com>
2022-10-22 16:38:46 +0300

    OOP1 projesi eklendi

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    public class ProductManager
+    {
+        public void Add(Product product)
+        {
+            Console.WriteLine("Ürün eklendi!\n"+"Ürün Adı: "+product.ProductName+"\n" +
+                "Ürün Fiyatı: "+product.Price+"\nStok Adedi: "+product.Stock);
+        }
+    
+    
+    
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product {
+                ID = 1,
+                CategoryID = 2,//mobilya kategorisi
+                ProductName = "Masa",
+                Price = 850,
+                Stock=12
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+
+
+
+
+
+
+
+
+        }
+    }
+}
