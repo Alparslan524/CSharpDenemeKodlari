c20f7d31765ff5863521e4aa5f9382728f026515
Alparslan <alparslanaydgn02@gmail.com>
2022-10-25 13:42:26 +0300

    Updated

--- OOP3/BasvuruManager.cs
+++ OOP3/BasvuruManager.cs
@@ -12,7 +12,8 @@ namespace OOP3
         }
 
         public void BasvuruYap(ICrediManager crediManager,ILog log)//Log işlemi olan BasvuruYap
-       {
+       {//Eğer bu şekilde interface parametresi göndermeseydik yeni bir kredi ekleyeceğimizde mavcut kodu bozmamız gerekiyordu
+            //Ve; ya kod tekrarı yapacaktık ya da onlarca if-else-method yazacaktık
             crediManager.Calculate();
             log.Logging();
         }//Method override
